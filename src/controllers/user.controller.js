894aa9b10e2bd08ad679c90869c9776fc1a3bd64
Gaj Singh Rathore <gajrathore353@gmail.com>
2024-06-20 22:28:24 +0530

    refresh access token controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -106,7 +106,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const { email, password, username } = req.body;
 
-  if (!username || !email) {
+  if (!(username || email)) {
     throw new ApiError(400, "username or email is required");
   }
 
@@ -171,9 +171,62 @@ const logoutUser = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .clearCookie("acessToken", options)
+    .clearCookie("accessToken", options)
     .clearCookie("refreshToken", options)
     .json(new ApiResponse(200,{},"User Logged Out"))
 });
 
+
+const refreshAccessToken=asyncHandler(async(req,res)=>{
+  const incomingRefreshToken=req.cookies.refreshToken || req.body.refreshToken
+
+  if(!incomingRefreshToken){
+    throw new ApiError(401,"unauthorized request")
+  }
+
+  try{
+    const decodedToken=jwt.verify(incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+
+    const user=await User.findById(decodedToken?._id)
+
+    if(!user){
+      throw new ApiError(401,"Invalid refresh token")
+    }
+
+    if(incomingRefreshToken!==user?.refreshToken){
+      throw new ApiError(401,"Refresh token is expired or used")
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true
+  }
+
+  const {accessToken, newRefreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", newRefreshToken, options)
+  .json(
+      new ApiResponse(
+          200, 
+          {accessToken, refreshToken: newRefreshToken},
+          "Access token refreshed"
+      )
+  )
+
+
+  }
+  catch(err){
+    throw new ApiError(401, error?.message || "Invalid refresh token")
+
+  }
+})
+
+
 export { registerUser, loginUser, logoutUser };
+
+
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,9 +1,9 @@
-import { ApiError } from "../utils/ApiError";
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-export const verifyJWT = asyncHandler(async (req, res, next) => {
+export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
