9d66c605ad07cce662c3940aa148f3fa90be429a
Gaj Singh Rathore <gajrathore353@gmail.com>
2024-06-20 17:03:40 +0530

    controller for registering user added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,7 +58,7 @@ userSchema.pre("save",async function(next){
 })
 
 userSchema.methods.isPasswordCorrect=async function(password){
-    return bcrypt.compare(password,this.password);
+    return await bcrypt.compare(password,this.password);
 }
 
 userSchema.methods.generateAccessToken=function(){
