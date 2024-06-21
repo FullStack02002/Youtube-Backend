51defb5f25fb9ee21d68f3ce6fcdf07b00d695f2
Gaj Singh Rathore <gajrathore353@gmail.com>
2024-06-21 20:06:51 +0530

    Controller for user channel profile added

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,8 @@ import {DB_NAME} from '../constants.js'
 
 const connectDB = async () => {
     try {
-        // const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        const connectionInstance = await mongoose.connect("mongodb+srv://gajrathore353:Gaj123@cluster0.mnul9xm.mongodb.net/youtube")
+        const connectionInstance= await mongoose.connect(`${process.env.MONGODBURI}/${DB_NAME}`);
+        // const connectionInstance = await mongoose.connect("mongodb+srv://gajrathore353:Gaj123@cluster0.mnul9xm.mongodb.net/youtube")
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
