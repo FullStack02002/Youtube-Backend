64324b534dab81f0539055585f37301600423636
Gaj Singh Rathore <gajrathore353@gmail.com>
2024-07-14 19:12:33 +0530

    Modified AysnHanler util

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,17 @@
 const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-    }
-}
+    return async (req, res, next) => {
+        try {
+            await Promise.resolve(requestHandler(req, res, next));
+        } catch (error) {
+            const statusCode = error.statusCode || 500;
+            const errorMessage = error.message || "Internal Server Error";
+            res.status(statusCode).json({
+                success: false,
+                error: errorMessage
+            });
+        }
+    };
+};
 
 
 export { asyncHandler }
