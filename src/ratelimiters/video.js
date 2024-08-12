d87468cf52a6aa7885a0f29f366d4a731fe6e5be
Gaj Singh Rathore <gajrathore353@gmail.com>
2024-08-12 21:36:55 +0530

    rate limit by video id added

--- src/ratelimiters/video.js
+++ src/ratelimiters/video.js
@@ -5,6 +5,8 @@ const incrementViewCountLimiter = rateLimit({
     windowMs: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
     max: 5, // Limit each IP to 10 requests per 24 hours
     message: "Too many requests from this IP to increment views, please try again later.",
+    keyGenerator: (req) => req.params.videoId, // Rate limit by video ID
+
   });
 
 
