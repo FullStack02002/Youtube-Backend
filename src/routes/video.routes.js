a7f7a2de206f430ca5b1566ea2a32dfcd0b12c81
Gaj Singh Rathore <gajrathore353@gmail.com>
2024-08-20 02:49:11 +0530

    change video publish route

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -22,21 +22,20 @@ import { Router } from "express";
 
 const router = Router();
 
-router
-  .route("/")
-  .get(getAllVideos)
-  .post(
-    verifyJWT,
-    upload.fields([
-      { name: "videoFile", maxCount: 1 },
-      { name: "thumbnail", maxCount: 1 },
-    ]),
-    publishAVideo
-  );
+router.route("/").get(getAllVideos);
+
+router.route("/publish").post(
+  verifyJWT,
+  upload.fields([
+    { name: "videoFile", maxCount: 1 },
+    { name: "thumbnail", maxCount: 1 },
+  ]),
+  publishAVideo
+);
 
 router
   .route("/v/:videoId")
-  .get( verifyJWT,getVideoById)
+  .get(verifyJWT, getVideoById)
   .delete(verifyJWT, deleteVideo)
   .patch(verifyJWT, upload.single("thumbnail"), updateVideo)
   .post(verifyJWT, incrementViewCountLimiter, incrementViewCount);
